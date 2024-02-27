97b3540aee863c30e3e9fc79a814c6176af28fe6
Vamsi Vytla <vamsi.vytla@gmail.com>
2024-02-26 16:01:26 -0800

    ip.py: Add two way buffering

--- liteeth/core/ip.py
+++ liteeth/core/ip.py
@@ -205,7 +205,7 @@ class LiteEthIPTX(Module):
         # # #
         # Fragmenter  .. TODO: Make it optional
         self.submodules.ip_fragmenter = ip_fragmenter = stream.BufferizeEndpoints(
-            {"source": stream.DIR_SOURCE})(LiteEthIPV4Fragmenter(dw))
+            {"sink": stream.DIR_SINK, "source": stream.DIR_SOURCE})(LiteEthIPV4Fragmenter(dw))
 
         # Checksum.
         self.submodules.checksum = checksum = LiteEthIPV4Checksum(skip_checksum=True)

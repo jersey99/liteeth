21dd9967e45062892c4066a1ed3859523bf3a7b5
Vamsi Vytla <vamsi.vytla@gmail.com>
2024-03-26 14:00:42 -0700

    core/__init__.py: Buffer VLANDepacketizer

--- liteeth/core/__init__.py
+++ liteeth/core/__init__.py
@@ -46,7 +46,8 @@ class LiteEthVLANUDPIPCore(Module, AutoCSR):
 
         self.submodules.crossbar     = LiteEthMACVLANCrossbar(dw)
         self.submodules.packetizer   = LiteEthMACVLANPacketizer(dw)
-        self.submodules.depacketizer = LiteEthMACVLANDepacketizer(dw)
+        self.submodules.depacketizer = LiteEthMACVLANDepacketizer(dw)stream.BufferizeEndpoints(
+            {"source": stream.DIR_SOURCE})()
 
         self.comb += [
             vlan_mac_port.sink.ethernet_type.eq(ethernet_8021q_tpid),

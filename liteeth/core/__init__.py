94ab240ebdc3f05e05a583a3ca0137e9ce86a115
Vamsi Vytla <vamsi.vytla@gmail.com>
2024-03-26 13:07:02 -0700

    core/__init__.py: Remove all buffering

--- liteeth/core/__init__.py
+++ liteeth/core/__init__.py
@@ -45,10 +45,8 @@ class LiteEthVLANUDPIPCore(Module, AutoCSR):
         vlan_mac_port = self.mac.crossbar.get_port(ethernet_8021q_tpid, dw=dw)
 
         self.submodules.crossbar     = LiteEthMACVLANCrossbar(dw)
-        self.submodules.packetizer   = stream.BufferizeEndpoints(
-            {"sink": stream.DIR_SINK})(LiteEthMACVLANPacketizer(dw))
-        self.submodules.depacketizer = stream.BufferizeEndpoints(
-            {"source": stream.DIR_SOURCE})(LiteEthMACVLANDepacketizer(dw))
+        self.submodules.packetizer   = LiteEthMACVLANPacketizer(dw)
+        self.submodules.depacketizer = LiteEthMACVLANDepacketizer(dw)
 
         self.comb += [
             vlan_mac_port.sink.ethernet_type.eq(ethernet_8021q_tpid),

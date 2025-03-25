50e9e7eb9267080556fc4013c5fda6700bf26f95
Vamsi Vytla <vamsi.vytla@gmail.com>
2025-03-24 20:55:54 -0700

    core/ip.py: Bufferize RX source side

--- liteeth/core/ip.py
+++ liteeth/core/ip.py
@@ -383,7 +383,8 @@ class LiteEthIP(LiteXModule):
     def __init__(self, mac, mac_address, ip_address, arp_table, with_broadcast=True, vlan_id=False, dw=8):
         self.tx = tx = stream.BufferizeEndpoints(
             {"source": stream.DIR_SOURCE}, pipe_ready=True)(LiteEthIPTX(mac_address, ip_address, arp_table, dw=dw))
-        self.rx = rx = LiteEthIPRX(mac_address, ip_address, with_broadcast, dw=dw)
+        self.rx = rx = stream.BufferizeEndpoints(
+            {"source": stream.DIR_SOURCE}, pipe_ready=True)(LiteEthIPRX(mac_address, ip_address, with_broadcast, dw=dw))
         if vlan_id is not False:
             assert(type(vlan_id) is Signal)
             mac_port = mac.crossbar.get_port((vlan_id << 16) | ethernet_type_ip, dw=dw)

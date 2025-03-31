88e9dfe1acbb33b6aa9197a9e0a287dcd6a9c8e4
Vamsi Vytla <vamsi.vytla@gmail.com>
2025-03-31 13:22:43 -0700

    ip.py: Fix bug inside Fragmenter. The initial condition was failing

--- liteeth/core/ip.py
+++ liteeth/core/ip.py
@@ -122,7 +122,7 @@ class LiteEthIPV4Fragmenter(LiteXModule):
         self.mf = mf = Signal(reset=0)  # mf == More Fragments
         self.fragment_offset = fragment_offset = Signal(13, reset=0)
         self.identification = identification = Signal(16, reset=0)
-        bytes_in_fragment = Signal(16, reset=0)
+        bytes_in_fragment = Signal(14, reset=0)
         # Making sure we only fragment in blocks of 8 bytes
         IP_MTU = ((eth_mtu - 30 - ipv4_header_length) >> 3) << 3
         self.fsm = fsm = FSM(reset_state="IDLE")
@@ -138,6 +138,7 @@ class LiteEthIPV4Fragmenter(LiteXModule):
                    ).Else(
                        sink.ready.eq(0),
                        counter_reset.eq(1),
+                       counter_reset_val.eq(IP_MTU >> 3),
                        source.length.eq(bytes_in_fragment),
                        NextValue(mf, 1),
                        NextValue(fragment_offset, 0),
